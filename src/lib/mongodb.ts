7a27583e30333468963cdc4ea334dae3b2ccfbca
nipun gupta <nipungupta4555@gmail.com>
2025-07-30 05:17:56 +0000

    reimplement mongo connectivity from scratch i gave you mongo env right

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -7,10 +7,6 @@ if (!uri) {
   throw new Error('Please add your Mongo URI to .env');
 }
 
-const options = {
-  tlsVersion: 'TLSv1.2',
-};
-
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
@@ -22,13 +18,13 @@ if (process.env.NODE_ENV === 'development') {
   }
 
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri);
     globalWithMongo._mongoClientPromise = client.connect();
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri);
   clientPromise = client.connect();
 }
 
