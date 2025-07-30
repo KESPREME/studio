2cea018e1a04c5f085c567ee64d987ddd5ccedc7
nipun gupta <nipungupta4555@gmail.com>
2025-07-30 05:17:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -7,6 +7,10 @@ if (!uri) {
   throw new Error('Please add your Mongo URI to .env');
 }
 
+const options = {
+  tlsVersion: 'TLSv1.2',
+};
+
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
@@ -18,13 +22,13 @@ if (process.env.NODE_ENV === 'development') {
   }
 
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, {});
+    client = new MongoClient(uri, options);
     globalWithMongo._mongoClientPromise = client.connect();
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, {});
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
