edb33c2917226fe930d39ef191479e5a5f730f29
nipun gupta <nipungupta4555@gmail.com>
2025-07-30 17:55:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
         hostname: 'placehold.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'zeycfgpgoptewbcyucxd.supabase.co',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
