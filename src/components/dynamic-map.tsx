747fa7e3b2d8bce9442a94af0a2eddfa2590d6ec
nipun gupta <nipungupta4555@gmail.com>
2025-07-30 05:01:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/dynamic-map.tsx
+++ src/components/dynamic-map.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import 'leaflet/dist/leaflet.css';
-import 'react-leaflet-cluster/lib/assets/MarkerCluster.css';
-import 'react-leaflet-cluster/lib/assets/MarkerCluster.Default.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
