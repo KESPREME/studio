7c6c1fa526bdceb66b3c6de261cdc3278cbbd295
nipun gupta <nipungupta4555@gmail.com>
2025-08-02 09:53:23 +0000

    connect to the firebase i said properly it aint working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCFvZ5__xwqc0JhWvU1TkqcVF-cveIFHYw",
   authDomain: "alertfront-czys9.firebaseapp.com",
   projectId: "alertfront-czys9",
-  storageBucket: "alertfront-czys9.appspot.com",
+  storageBucket: "alertfront-czys9.firebasestorage.app",
   messagingSenderId: "752668007574",
   appId: "1:752668007574:web:062c2403d39499bd30f6b7"
 };
