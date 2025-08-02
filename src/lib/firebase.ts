049c7d16d04efd5ab47c32091a42d76e296a8bd9
nipun gupta <nipungupta4555@gmail.com>
2025-08-02 09:47:20 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: "alertfront-czys9",
-  appId: "1:752668007574:web:062c2403d39499bd30f6b7",
-  storageBucket: "alertfront-czys9.appspot.com",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: "AIzaSyCFvZ5__xwqc0JhWvU1TkqcVF-cveIFHYw",
   authDomain: "alertfront-czys9.firebaseapp.com",
-  messagingSenderId: "752668007574"
+  projectId: "alertfront-czys9",
+  storageBucket: "alertfront-czys9.appspot.com",
+  messagingSenderId: "752668007574",
+  appId: "1:752668007574:web:062c2403d39499bd30f6b7"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
